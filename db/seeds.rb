0380218405e5c43cd71b6f0d99e6aa821eec4e2d
Yoshi3lines <ny3line@gmail.com>
2021-05-02 16:18:32 +0900

    115のbootstrapまで完了

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Question.create(id: 1, name: 'test name1', title: 'test title1', content: 'test content1')
+Question.create(id: 2, name: 'test name2', title: 'test title2', content: 'test content2')
+Question.create(id: 3, name: 'test name3', title: 'test title3', content: 'test content3')
